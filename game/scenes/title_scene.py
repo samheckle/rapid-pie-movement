24a2ba2be2910a77b6b442270bdbcb0fba79f170
zujko <peter@zujko.xyz>
2017-04-29 16:17:52 -0400

    Initial buttons

--- game/scenes/title_scene.py
+++ game/scenes/title_scene.py
@@ -1,5 +1,6 @@
 import pygame
 from . import *
+from .tutorial_scene import TutorialScene
 
 class TitleScene(BaseScene):
     """
@@ -8,12 +9,30 @@ class TitleScene(BaseScene):
     """
     def __init__(self, context):
         BaseScene.__init__(self, context)
+        self.context.screen.blit(self.context.title,(0,0))
+        self.create_buttons()
 
     def handle_inputs(self, events):
         # Handle events
         for event in events:
-            pass
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                if self.start_box.collidepoint(pos):
+                    self.context.scene = GameScene(self.context)
+
+                elif self.help_box.collidepoint(pos):
+                    self.context.scene = TutorialScene(self.context)
+
+    def create_buttons(self):
+        self.start_btn = pygame.Surface((436,104), pygame.SRCALPHA, 32)
+        self.start_btn.convert_alpha()
+        self.start_btn.blit(self.context.startbtn, (0,0))
+        self.start_box = self.context.screen.blit(self.start_btn, (400,500))
+
+        self.tut_btn = pygame.Surface((436,104), pygame.SRCALPHA, 32)
+        self.tut_btn.convert_alpha()
+        self.tut_btn.blit(self.context.howtobtn, (0,0))
+        self.help_box = self.context.screen.blit(self.tut_btn, (400, 700))
             
     def render_scene(self):
-        self.context.screen.blit(self.context.title,(0,0))
         pygame.display.flip()
